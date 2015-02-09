b9b8aa5e84e175c94623b5c1b1c47527a9b8a3d9
Ruiyu Xu <rheaxu90@gmail.com>
2015-02-09 17:20:03 -0500

    Finished simple contactlist app -- angularjs, mongodb, nodejs, express

--- contactlist/public/controllers/controller.js
+++ contactlist/public/controllers/controller.js
@@ -7,8 +7,46 @@ angular.module('myApp',[]).controller('AppCtrl',['$scope','$http',AppCtrl]);
 
 function AppCtrl($scope,$http){
 
-	$http.get("/contactlist").success(function(response){
-		console.log("I got the data I requested");
-		$scope.contactlist = response;
-	});
+	var refresh = function(){
+		$http.get("/contactlist").success(function(response){
+			console.log("I got the data I requested");
+			$scope.contactlist = response;
+			$scope.contact = "";
+		});
+	};
+
+	refresh();	//get the data right when we load the page
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		$http.post("/contactlist",$scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};
+
+	$scope.remove = function(id){
+		console.log(id);
+		$http.delete("/contactlist/"+id).success(function(reponse){
+			refresh();
+		});
+
+	};
+
+	$scope.edit = function(id){
+		console.log(id);
+		$http.get("/contactlist/"+id).success(function(response){
+			$scope.contact=response;
+		});
+	};
+
+	$scope.update = function(){
+		console.log($scope.contact._id);
+		$http.put("/contactlist/"+$scope.contact._id,$scope.contact).success(function(response){
+			refresh();
+		});
+	}
+
+	$scope.deselect = function(){
+		$scope.contact = "";
+	}
 }
--- contactlist/server.js
+++ contactlist/server.js
@@ -1,12 +1,60 @@
 var express = require("express");
 var app = express();
+var mongojs = require("mongojs");
+var db = mongojs("contactlist",["contactlist"]);	//which mongodb database and colleciton will be used
+var bodyParser = require("body-parser");
 
 /*app.get("/",function(req,res){
 	res.send("Hello World from server.js")
 });*/
 
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.json());
 
+/*This method get data from database*/
+app.get("/contactlist",function(req,res){
+	console.log("I received a get request");
+
+	db.contactlist.find(function(err,docs){
+		console.log(docs);
+		res.json(docs);
+	});
+})
+
+app.post("/contactlist",function(req,res){
+	console.log(req.body);	//get data from the body of request. But the server doesn't know how to parse it yet.
+	db.contactlist.insert(req.body,function(err,doc){	// the function is the callback to be run after the records are updated.
+		res.json(doc);	//send json data back to controller
+	});
+})
+
+app.delete("/contactlist/:id",function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);	//doc is the item we are removing
+	});
+});
+
+app.get("/contactlist/:id",function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+});
+
+app.put("/contactlist/:id",function(req,res){
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify({query:{_id:mongojs.ObjectId(id)},
+		update:{$set:{name:req.body.name,email:req.body.email,number:req.body.number}},
+		new: true},function(err,doc){
+			res.json(doc);
+		});
+})
+
+/* This method doesn't get data from database
 app.get("/contactlist",function(req,res){
 	console.log("I received a get request")	//This msg will be print on the server
 
@@ -30,7 +78,7 @@ app.get("/contactlist",function(req,res){
 
 	var contactlist = [person1,person2,person3];
 	res.json(contactlist);
-})
+})*/
 
 app.listen(3000);
 console.log("Server running on port 3000");
