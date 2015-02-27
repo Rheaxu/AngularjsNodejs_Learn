9073c11594f2599d8feea119c3067e618e2b4d4b
Ruiyu Xu <rheaxu90@gmail.com>
2015-02-26 22:42:04 -0500

    start route

---
+++ route/data.js
@@ -0,0 +1,90 @@
+{
+        "fname": "Rebecca",
+        "lname": "Leon",
+        "tel": "(106)260-9746",
+        "address": "5952 Magna Rd",
+        "city": "Cambridge",
+        "state": "MN",
+        "zip": "63182"
+    },
+    {
+        "fname": "Johanan",
+        "lname": "Altermatt",
+        "tel": "(763)944-8723",
+        "address": "4518 Dolor St",
+        "city": "Hudsonville",
+        "state": "LA",
+        "zip": "10035"
+    },
+    {
+        "fname": "Carol",
+        "lname": "Kasuganti",
+        "tel": "(900)371-0579",
+        "address": "8145 Amet Dr",
+        "city": "Woodlawn",
+        "state": "MI",
+        "zip": "58937"
+    },
+    {
+        "fname": "Alexis",
+        "lname": "Levin",
+        "tel": "(861)270-7918",
+        "address": "8848 Vitae Ave",
+        "city": "Mount Morris",
+        "state": "KS",
+        "zip": "81860"
+    },
+    {
+        "fname": "Fredrick",
+        "lname": "Valkanet",
+        "tel": "(330)093-4000",
+        "address": "47 Ac Ave",
+        "city": "Carrollton",
+        "state": "CT",
+        "zip": "13491"
+    },
+    {
+        "fname": "Josephine",
+        "lname": "Cowen",
+        "tel": "(435)794-0911",
+        "address": "469 Aenean Ln",
+        "city": "Durham",
+        "state": "VA",
+        "zip": "35339"
+    },
+    {
+        "fname": "Alla",
+        "lname": "Rannila",
+        "tel": "(898)388-7902",
+        "address": "1618 Non St",
+        "city": "Prattville",
+        "state": "WY",
+        "zip": "67682"
+    },
+    {
+        "fname": "Abraham",
+        "lname": "Cook",
+        "tel": "(917)891-8752",
+        "address": "5166 Libero Rd",
+        "city": "Larimore",
+        "state": "KS",
+        "zip": "25070"
+    },
+    {
+        "fname": "Rone",
+        "lname": "Haaf",
+        "tel": "(981)383-1233",
+        "address": "4296 Placerat Ct",
+        "city": "Vero Beach",
+        "state": "VT",
+        "zip": "18811"
+    },
+    {
+        "fname": "Graeme",
+        "lname": "Koskovich",
+        "tel": "(375)632-1371",
+        "address": "6052 Etiam Ln",
+        "city": "Circleville",
+        "state": "NY",
+        "zip": "89672"
+    }
